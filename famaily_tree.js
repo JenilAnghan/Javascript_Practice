d4b6b46234690e1c66b287161340a7c0b28248f0
Jenil Anghan <jenilanghan508@gmail.com>
2023-09-10 12:59:24 +0530

    add data

---
+++ famaily_tree.js
@@ -0,0 +1,86 @@
+let familytree = {
+    name: "Tulshibhai",
+    age: 73,
+    child: [
+        {
+            id: 10001,
+            name: "Rameshbhai",
+            age: 52,
+            occupation: "Bussiness",
+            vehicle: [
+                {
+                    name: "shine",
+                    number: "0000",
+                },
+                {
+                    name: "passion",
+                    number: "0000"
+                },
+            ],
+            son: [
+                {
+                    name: "jenil",
+                    age:"20",
+                },
+                {
+                    name: "parth",
+                    age:"23",
+                },
+            ],
+        },
+
+        {
+            id: 10002,
+            name: "Rameshbhai",
+            age: 52,
+            occupation: "Bussiness",
+            vehicle: [
+                {
+                    name: "Thar",
+                    number: "0000",
+                },
+                {
+                    name: "passion",
+                    number: "0000"
+                },
+            ],
+            son: [
+                {
+                    name: "krish",
+                    age:"20",
+                },
+                {
+                    name: "zeel",
+                    age:"23",
+                },
+            ],
+        },
+
+        {
+            id: 10001,
+            name: "Rameshbhai",
+            age: 52,
+            occupation: "Bussiness",
+            vehicle: [
+                {
+                    name: "honda",
+                    number: "0000",
+                },
+                {
+                    name: "passion",
+                    number: "0000"
+                },
+            ],
+            son: [
+                {
+                    name: "jay",
+                    age:"20",
+                },
+                {
+                    name: "meet",
+                    age:"23",
+                },
+            ],
+        },
+    ]
+}

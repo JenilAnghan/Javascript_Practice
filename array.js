b591ea97380c82685b16c0392a701da4fa831f18
jenil <jenilpatel7741@gmail.com>
2023-10-11 21:53:00 +0530

    array

---
+++ array.js
@@ -0,0 +1,27 @@
+// [1,2,3,300,100]
+// -> create array which have 1 to 9 no
+
+// 1. print 9th and 10th index of array
+
+
+// let Array = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+
+// console.log(Array[8]);
+// console.log(Array[9]);
+
+
+// 2. print every element of array by map and for loop
+
+// const Array = [1,2,3,4,5,6,7,8,9];
+
+// Array.map((element) => {
+//     console.log("element",element);
+// });
+
+
+// const Array1 = [1,2,3,4,5,6,7,8,9];
+
+// for (let i = 0; i < Array1.length; i++){
+//     console.log(Array1[i])
+// }
+

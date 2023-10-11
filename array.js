6d0de36828a76a8223209c392e29263d8ce5ca8f
jenil <jenilpatel7741@gmail.com>
2023-10-11 22:02:35 +0530

    array

--- array.js
+++ array.js
@@ -3,12 +3,10 @@
 
 // 1. print 9th and 10th index of array
 
-
 // let Array = [0,1,2,3,4,5,6,7,8,9,10,11,12];
 
 // console.log(Array[8]);
 // console.log(Array[9]);
-
 
 // 2. print every element of array by map and for loop
 
@@ -18,28 +16,21 @@
 //     console.log("element",element);
 // });
 
-
 // const Array1 = [1,2,3,4,5,6,7,8,9];
 
 // for (let i = 0; i < Array1.length; i++){
 //     console.log(Array1[i])
 // }
 
-
-
 // 3. change a value of 6th index with 100
 
 // const Array = [1,2,3,4,5,6,7,8,9];
- 
 
 // Array[9] = 90;
 
 // console.log(Array)
 
-
 // 4. print a array till element value not grater then 100
-
-
 
 // const Array = [20, 30, 65, 100, 90, 80, 10, 120];
 
@@ -49,3 +40,11 @@
 //   }
 //   console.log(Array[i]);
 // }
+
+// 5. add 1 in every element of an array
+
+// const Array = [1, 2, 3, 4, 5];
+
+// const Array1 = Array.map((element) => element + 1);
+
+// console.log(Array1);

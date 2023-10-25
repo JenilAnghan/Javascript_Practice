05ab7193518865eaddfc04aef8b2d3c4993fec94
jenil <jenilpatel7741@gmail.com>
2023-10-25 11:23:31 +0530

    famaily-tree

--- famaily_tree.js
+++ famaily_tree.js
@@ -1,88 +1,88 @@
 let familytree = {
-    name: "Tulshibhai",
-    age: 73,
-    child: [
+  name: "Tulshibhai",
+  age: 73,
+  child: [
+    {
+      id: 10001,
+      name: "Rameshbhai",
+      age: 52,
+      occupation: "Bussiness",
+      vehicle: [
         {
-            id: 10001,
-            name: "Rameshbhai",
-            age: 52,
-            occupation: "Bussiness",
-            vehicle: [
-                {
-                    name: "shine",
-                    number: "0000",
-                },
-                {
-                    name: "passion",
-                    number: "0000"
-                },
-            ],
-            son: [
-                {
-                    name: "jenil",
-                    age:"20",
-                },
-                {
-                    name: "parth",
-                    age:"23",
-                },
-            ],
+          name: "shine",
+          number: "0000",
         },
-
         {
-            id: 10002,
-            name: "Rameshbhai",
-            age: 52,
-            occupation: "Bussiness",
-            vehicle: [
-                {
-                    name: "Thar",
-                    number: "0000",
-                },
-                {
-                    name: "passion",
-                    number: "0000"
-                },
-            ],
-            son: [
-                {
-                    name: "krish",
-                    age:"20",
-                },
-                {
-                    name: "zeel",
-                    age:"23",
-                },
-            ],
+          name: "passion",
+          number: "0000",
         },
-
+      ],
+      son: [
         {
-            id: 10001,
-            name: "Rameshbhai",
-            age: 52,
-            occupation: "Bussiness",
-            vehicle: [
-                {
-                    name: "honda",
-                    number: "0000",
-                },
-                {
-                    name: "passion",
-                    number: "0000"
-                },
-            ],
-            son: [
-                {
-                    name: "jay",
-                    age:"20",
-                },
-                {
-                    name: "meet",
-                    age:"23",
-                },
-            ],
+          name: "jenil",
+          age: "20",
         },
-    ]
-}
+        {
+          name: "parth",
+          age: "23",
+        },
+      ],
+    },
 
-console.log("familytree",familytree)
+    {
+      id: 10002,
+      name: "Rameshbhai",
+      age: 52,
+      occupation: "Bussiness",
+      vehicle: [
+        {
+          name: "Thar",
+          number: "0000",
+        },
+        {
+          name: "passion",
+          number: "0000",
+        },
+      ],
+      son: [
+        {
+          name: "krish",
+          age: "20",
+        },
+        {
+          name: "zeel",
+          age: "23",
+        },
+      ],
+    },
+
+    {
+      id: 10001,
+      name: "Rameshbhai",
+      age: 52,
+      occupation: "Bussiness",
+      vehicle: [
+        {
+          name: "honda",
+          number: "0000",
+        },
+        {
+          name: "passion",
+          number: "0000",
+        },
+      ],
+      son: [
+        {
+          name: "jay",
+          age: "20",
+        },
+        {
+          name: "meet",
+          age: "23",
+        },
+      ],
+    },
+  ],
+};
+
+console.log("familytree", familytree);
